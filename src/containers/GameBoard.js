6491cee48c8b3d9c3faedd487efc1eaf68e1863d
Jim <jameslotruglio@gmail.com>
2024-04-28 15:27:38 -0400

    Moved GameBoard into its own component

---
+++ src/containers/GameBoard.js
@@ -0,0 +1,50 @@
+
+
+const GameBoard = ({rowClasses, rowValues}) => {
+
+    return (
+        <div className="attempts">
+            <table className="attempt-table">
+                <tbody>
+                    <tr className="table-row">
+                        <td className={`attempt-cell ` + rowClasses[0][0]}>{rowValues[0][0]}</td>
+                        <td className={`attempt-cell ` + rowClasses[0][1]}>{rowValues[0][1]}</td>
+                        <td className={`attempt-cell ` + rowClasses[0][2]}>{rowValues[0][2]}</td>
+                        <td className={`attempt-cell ` + rowClasses[0][3]}>{rowValues[0][3]}</td>
+                        <td className={`attempt-cell ` + rowClasses[0][4]}>{rowValues[0][4]}</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className={`attempt-cell ` + rowClasses[1][0]}>{rowValues[1][0]}</td>
+                        <td className={`attempt-cell ` + rowClasses[1][1]}>{rowValues[1][1]}</td>
+                        <td className={`attempt-cell ` + rowClasses[1][2]}>{rowValues[1][2]}</td>
+                        <td className={`attempt-cell ` + rowClasses[1][3]}>{rowValues[1][3]}</td>
+                        <td className={`attempt-cell ` + rowClasses[1][4]}>{rowValues[1][4]}</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className={`attempt-cell ` + rowClasses[2][0]}>{rowValues[2][0]}</td>
+                        <td className={`attempt-cell ` + rowClasses[2][1]}>{rowValues[2][1]}</td>
+                        <td className={`attempt-cell ` + rowClasses[2][2]}>{rowValues[2][2]}</td>
+                        <td className={`attempt-cell ` + rowClasses[2][3]}>{rowValues[2][3]}</td>
+                        <td className={`attempt-cell ` + rowClasses[2][4]}>{rowValues[2][4]}</td>
+                    </tr>
+                    <tr>
+                        <td className={`attempt-cell ` + rowClasses[3][0]}>{rowValues[3][0]}</td>
+                        <td className={`attempt-cell ` + rowClasses[3][1]}>{rowValues[3][1]}</td>
+                        <td className={`attempt-cell ` + rowClasses[3][2]}>{rowValues[3][2]}</td>
+                        <td className={`attempt-cell ` + rowClasses[3][3]}>{rowValues[3][3]}</td>
+                        <td className={`attempt-cell ` + rowClasses[3][4]}>{rowValues[3][4]}</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className={`attempt-cell ` + rowClasses[4][0]}>{rowValues[4][0]}</td>
+                        <td className={`attempt-cell ` + rowClasses[4][1]}>{rowValues[4][1]}</td>
+                        <td className={`attempt-cell ` + rowClasses[4][2]}>{rowValues[4][2]}</td>
+                        <td className={`attempt-cell ` + rowClasses[4][3]}>{rowValues[4][3]}</td>
+                        <td className={`attempt-cell ` + rowClasses[4][4]}>{rowValues[4][4]}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default GameBoard
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import GameBoard from "./GameBoard"
 
 
 const Home = () => {
@@ -24,7 +25,7 @@ const Home = () => {
         letterFour: "",
         letterFive: ""
     })
-    // const [rowOneClass, setRowOneClass] = useState("letter-present")
+
     const {letterOne, letterTwo, letterThree, letterFour, letterFive} = formData
     const [currentRow, setCurrentRow] = useState(0)
     const [currentWord, setCurrentWord] = useState(["B", "L", "A", "D", "E"])
@@ -67,50 +68,7 @@ const Home = () => {
     return (
         <div>
             <h1>Wordly</h1>
-
-            <div className="attempts">
-                <table className="attempt-table">
-                    <tbody>
-                        <tr className="table-row">
-                            <td className={`attempt-cell ` + rowClasses[0][0]}>{rowValues[0][0]}</td>
-                            <td className={`attempt-cell ` + rowClasses[0][1]}>{rowValues[0][1]}</td>
-                            <td className={`attempt-cell ` + rowClasses[0][2]}>{rowValues[0][2]}</td>
-                            <td className={`attempt-cell ` + rowClasses[0][3]}>{rowValues[0][3]}</td>
-                            <td className={`attempt-cell ` + rowClasses[0][4]}>{rowValues[0][4]}</td>
-                        </tr>
-                        <tr className="table-row">
-                            <td className={`attempt-cell ` + rowClasses[1][0]}>{rowValues[1][0]}</td>
-                            <td className={`attempt-cell ` + rowClasses[1][1]}>{rowValues[1][1]}</td>
-                            <td className={`attempt-cell ` + rowClasses[1][2]}>{rowValues[1][2]}</td>
-                            <td className={`attempt-cell ` + rowClasses[1][3]}>{rowValues[1][3]}</td>
-                            <td className={`attempt-cell ` + rowClasses[1][4]}>{rowValues[1][4]}</td>
-                        </tr>
-                        <tr className="table-row">
-                            <td className={`attempt-cell ` + rowClasses[2][0]}>{rowValues[2][0]}</td>
-                            <td className={`attempt-cell ` + rowClasses[2][1]}>{rowValues[2][1]}</td>
-                            <td className={`attempt-cell ` + rowClasses[2][2]}>{rowValues[2][2]}</td>
-                            <td className={`attempt-cell ` + rowClasses[2][3]}>{rowValues[2][3]}</td>
-                            <td className={`attempt-cell ` + rowClasses[2][4]}>{rowValues[2][4]}</td>
-                        </tr>
-                        <tr>
-                            <td className={`attempt-cell ` + rowClasses[3][0]}>{rowValues[3][0]}</td>
-                            <td className={`attempt-cell ` + rowClasses[3][1]}>{rowValues[3][1]}</td>
-                            <td className={`attempt-cell ` + rowClasses[3][2]}>{rowValues[3][2]}</td>
-                            <td className={`attempt-cell ` + rowClasses[3][3]}>{rowValues[3][3]}</td>
-                            <td className={`attempt-cell ` + rowClasses[3][4]}>{rowValues[3][4]}</td>
-                        </tr>
-                        <tr className="table-row">
-                            <td className={`attempt-cell ` + rowClasses[4][0]}>{rowValues[4][0]}</td>
-                            <td className={`attempt-cell ` + rowClasses[4][1]}>{rowValues[4][1]}</td>
-                            <td className={`attempt-cell ` + rowClasses[4][2]}>{rowValues[4][2]}</td>
-                            <td className={`attempt-cell ` + rowClasses[4][3]}>{rowValues[4][3]}</td>
-                            <td className={`attempt-cell ` + rowClasses[4][4]}>{rowValues[4][4]}</td>
-                        </tr>
-
-                    </tbody>
-                    
-                </table>
-            </div>
+                <GameBoard rowClasses={rowClasses} rowValues={rowValues} />
 
             <div className="input-containers">
                 <form onSubmit={handleSubmit}>
@@ -156,10 +114,7 @@ const Home = () => {
                         /><br />
                     <button className="btn" type="submit">Submit</button>
                 </form>
-                
-
             </div>
-            
         </div>
     )
 }
